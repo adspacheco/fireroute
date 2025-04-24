7ba70fe22a87cb293e032019753e448e3628c635
adspacheco <andersonac7@outlook.com>
2025-04-23 22:32:12 -0300

    fix: update map view to follow marker position changes

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -1,7 +1,7 @@
 "use client";
-
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { LatLngExpression, LatLngTuple } from "leaflet";
+import { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
@@ -10,6 +10,22 @@ export interface MapProps {
   position: LatLngExpression | LatLngTuple;
   zoom?: number;
   className?: string;
+}
+
+function ChangeView({
+  position,
+  zoom,
+}: {
+  position: LatLngExpression;
+  zoom: number;
+}) {
+  const map = useMap();
+
+  useEffect(() => {
+    map.setView(position, zoom);
+  }, [map, position, zoom]);
+
+  return null;
 }
 
 const DEFAULT_ZOOM = 15;
@@ -23,6 +39,8 @@ export const Map = ({ position, zoom = DEFAULT_ZOOM, className }: MapProps) => {
       className={`${className} leaflet-container-relative`}
       doubleClickZoom={true}
     >
+      <ChangeView position={position} zoom={zoom} />
+
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
